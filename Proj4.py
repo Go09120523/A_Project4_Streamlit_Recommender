ebc683547a6e1db7675c34c4054e9fc95be95875
Go09120523 <101860533+Go09120523@users.noreply.github.com>
2022-03-19 05:17:13 +0700

    Add files via upload

---
+++ Proj4.py
@@ -0,0 +1,10 @@
+# Import packages---------------------------------------------------------------
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import streamlit as st
+import pickle
+# import warnings
+# warnings.filterwarnings('ignore')
+st.write('Hello')
