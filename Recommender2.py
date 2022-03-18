c3dd896def6cfac8e1d66bc08298148ef45e9c39
Go09120523 <101860533+Go09120523@users.noreply.github.com>
2022-03-19 04:52:47 +0700

    Add files via upload

--- Recommender2.py
+++ Recommender2.py
@@ -7,11 +7,12 @@ import streamlit as st
 import pickle
 # import warnings
 # warnings.filterwarnings('ignore')
-
-pd.options.display.float_format = '{:.2f}'.format
-st.set_page_config(layout="wide")
-st.set_option('deprecation.showPyplotGlobalUse', False)
 st.write('Hello')
+
+# pd.options.display.float_format = '{:.2f}'.format
+# st.set_page_config(layout="wide")
+# st.set_option('deprecation.showPyplotGlobalUse', False)
+
 # # Input files----------------------------------------------------------------------
 # products = pd.read_csv('Product_new.csv')
 # reviews = pd.read_csv('Review_new.zip', lineterminator='\n')
